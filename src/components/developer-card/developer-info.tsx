9e249092e8955ed9a6a864f0bc943b182e4b26b5
Jackie <jackiecai90@gmail.com>
2024-05-05 13:28:41 +1000

    replaced with new resume and added some colons

--- src/components/developer-card/developer-info.tsx
+++ src/components/developer-card/developer-info.tsx
@@ -5,23 +5,23 @@ const DeveloperInfo: FC<{small?: boolean}> = (({ small = false }) => (
   <>
     <RowGroup>
       <Row small={small}>
-        <span>Name</span>
+        <span>Name:</span>
         <span>Jackie Cai</span>
       </Row>
     </RowGroup>
     <RowGroup>
       <Row small={small}>
-        <span>University</span>
+        <span>University:</span>
         <span>University of New South Wales</span>
       </Row>
       <Row small={small}>
-        <span>Degree</span>
+        <span>Degree:</span>
         <span>Computer Science & Econometrics</span>
       </Row>
     </RowGroup>
     <RowGroup>
       <Row small={small}>
-        <span>Favourite Pokemon</span>
+        <span>Favourite Pokemon:</span>
         <span>Aegislash</span>
       </Row>
     </RowGroup>
