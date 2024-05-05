3c5c96f1a5fda54f83e5e76d8d299ecea8311280
Jackie <jackiecai90@gmail.com>
2024-05-05 23:08:27 +1000

    changed the avatar a bit and changed the degree and uni name for the mobile verison

--- src/components/developer-card/developer-info.tsx
+++ src/components/developer-card/developer-info.tsx
@@ -12,11 +12,11 @@ const DeveloperInfo: FC<{small?: boolean}> = (({ small = false }) => (
     <RowGroup>
       <Row small={small}>
         <span>University:</span>
-        <span>University of New South Wales</span>
+        <span>UNSW Sydney</span>
       </Row>
       <Row small={small}>
         <span>Degree:</span>
-        <span>Computer Science & Econometrics</span>
+        <span>Computer Science & Economics</span>
       </Row>
     </RowGroup>
     <RowGroup>
