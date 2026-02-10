5638fccfa315bd1ba906a810b2bbe7c32088865d
Jackie <jackiecai90@gmail.com>
2026-02-11 02:01:54 +1100

    added to the index card MS

--- src/experiences/index.tsx
+++ src/experiences/index.tsx
@@ -1,10 +1,12 @@
 import { Pwc } from './pwc';
 import { Experience, ExperienceName } from '../types/experience';
 import { Macquarie } from './macquarie';
+import { Microsoft } from './microsoft';
 
 const experiencesOrder = [
   Pwc,
   Macquarie,
+  Microsoft,
 ];
 
 export const experiences: {[key in ExperienceName as key]: Experience} = experiencesOrder.reduce((a, exp, idx) => ({
